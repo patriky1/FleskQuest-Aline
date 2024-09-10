89577d861f60105b4e80f563a1a5a48901e6095c
patriky1 <patrikybrito@gmail.com>
2024-09-09 23:21:51 -0300

    first commit

--- flaskquestia/settings.py
+++ flaskquestia/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-05s8@d&$dspu_+i7_ro4slc(t8xr#g5l$c*-1$a_xsvjg@xw4=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
