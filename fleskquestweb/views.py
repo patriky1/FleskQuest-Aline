69c49c88b3fe203bcea270069e8b2482fe65867f
patriky1 <patrikybrito@gmail.com>
2024-09-13 00:48:09 -0300

    fix: search on clear

--- fleskquestweb/views.py
+++ fleskquestweb/views.py
@@ -4,7 +4,6 @@ from .forms import PerguntaForm
 from django.conf import settings
 import json
 # import markdown
-from django import template
 # register = template.Library()
 # Configurar a chave da API do Google Gemini
 genai.configure(api_key=settings.GOOGLE_GEMINI_API_KEY)
@@ -27,6 +26,8 @@ def index(request):
     if request.method == 'POST':
         form = PerguntaForm(request.POST)
         if form.is_valid():
+            #verificar se está funcionando corretamente a linha abaixo
+            request.session['conversas'] = []
             pergunta = form.cleaned_data['pergunta']
             # Adiciona o texto pré-definido à pergunta do usuário
             prompt = texto_pre_definido + pergunta
@@ -45,6 +46,7 @@ def index(request):
             request.session.modified = True  # Marca a sessão como modificada para garantir que seja salva
     else:
         form = PerguntaForm()
+    
 
     return render(request, 'index.html', {'form': form, 'conversas': request.session['conversas']})
 ##converter markdown para html
